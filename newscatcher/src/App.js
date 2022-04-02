f6e00522d199fad4dc360d246b837872a6feee03
yogitheboss <yograjrjpt123@gmail.com>
2022-04-02 23:07:08 +0530

    This is a news Tracker web-app

---
+++ newscatcher/src/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./App.css";
+export default class App extends Component {
+  render() {
+    let  c = "yogi";
+    return <div>Hello my first class based component {c}</div>;
+  }
+}
