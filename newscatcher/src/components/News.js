28b44c9242bcd8b817f73ca01c20b39c7fbb625f
yogitheboss <yograjrjpt123@gmail.com>
2022-04-03 23:59:53 +0530

    news-api fetching using component did mount
    also added buttons to the site for next and previous
    also used createRoot instead of render as react 18 deprecated the use of render

--- newscatcher/src/components/News.js
+++ newscatcher/src/components/News.js
@@ -1,26 +1,68 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export default class News extends Component {
+  articles=[];
+  
+  constructor() {
+    super();
+    this.state = {
+      articles: this.articles,
+      loading: false,
+      page: 1,
+      
+    };
+  }
+  handleNextClick=()=>{
+    console.log("hello bhai")
+  }
+  handlePrevClick=()=>{
+    console.log("hello bhai")
+  }
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=55c7e6a22d60449892bca4b70dca2cb6";
+    let data = await fetch(url);
+    let parseData = await data.json();
+    console.log(parseData);
+    this.setState({ articles: parseData.articles });
+  }
   render() {
+   
     return (
       <div>
-          <div className="container my-3">
-          <h2>News Catcher</h2>
-          <div className="row">
-              <div className="col-md-4">
-              <NewsItem title="MyTitle" description="myDesc"/>
-              </div>
-              <div className="col-md-4">
-              <NewsItem/>
-              </div>
-              <div className="col-md-4">
-              <NewsItem/>
-              </div>
-         
+        <div className="container my-3">
+          <h2 className="text-center my-3">News Catcher- Top Headlines</h2>
+          <div className="row my-3">
+            <div className="row my-3">
+              {this.state.articles.map((element) => {
+                return (
+                  <div className="col-md-4" key={element.url}>
+                    <NewsItem
+                      title={element.title ? element.title.slice(0, 45) : ""}
+                      description={
+                        element.description
+                          ? element.description.slice(0, 80)
+                          : ""
+                      }
+                      imgUrl={element.urlToImage}
+                      newsUrl={element.url}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+            <div className="container d-flex justify-content-between">
+              <button type="button" className="btn btn-dark" onClick={this.handlePrevClick}>
+              &larr; Previous
+              </button>
+              <button type="button" className="btn btn-dark mx-3" onClick={this.handleNextClick}>
+                Next	&rarr;              
+              </button>
+            </div>
           </div>
-          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
--- newscatcher/src/components/NewsItem.js
+++ newscatcher/src/components/NewsItem.js
@@ -1,18 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class NewsItem extends Component {
-  render() {
-      let {title,description}=this.props;
-    return (
-      <div><div className="card">
-      <img src="https://www.gannett-cdn.com/presto/2022/04/02/USAT/1b36f218-2b6c-4bb9-b5fb-2d5e89dd48ee-AP_Russia_Ukraine_War.jpg?auto=webp&crop=3413,1920,x0,y96&format=pjpg&width=1200" className="card-img-top" alt="..."/>
-    
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" className="btn btn-primary">Go somewhere</a>
+  render(){
+  let { title, description, imgUrl ,newsUrl} = this.props;
+  return (
+      <div className="my-3">
+        <div className="card">
+          <img src={imgUrl?imgUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzVoxHsNEztGWOV4UN7UKLVbfkPGp14t8iSgrpScY&s"} className="card-img-top" alt="..." />
+
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a href={newsUrl} rel="noreferrer" className="btn btn-dark ">
+            Read more
+            </a>
+          </div>
+        </div>
       </div>
-    </div></div>
-    )
+    );
   }
 }
