1e40ccb33eb27c10919eef35f9bd3bb0d85e3bfd
yogitheboss <yograjrjpt123@gmail.com>
2022-04-04 10:16:44 +0530

    fetching directly from news-api

--- newscatcher/src/components/News.js
+++ newscatcher/src/components/News.js
@@ -2,23 +2,34 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export default class News extends Component {
-  articles=[];
-  
+  articles = [];
+
   constructor() {
     super();
     this.state = {
       articles: this.articles,
       loading: false,
       page: 1,
-      
     };
   }
-  handleNextClick=()=>{
-    console.log("hello bhai")
-  }
-  handlePrevClick=()=>{
-    console.log("hello bhai")
-  }
+  handleNextClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=55c7e6a22d60449892bca4b70dca2cb6&page=${
+      this.state.page + 1
+    }`;
+    let data = await fetch(url);
+    let parseData = await data.json();
+    console.log(parseData);
+    this.setState({ page: this.state.page + 1, articles: parseData.articles });
+  };
+  handlePrevClick =async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=55c7e6a22d60449892bca4b70dca2cb6&page=${
+      this.state.page - 1
+    }`;
+    let data = await fetch(url);
+    let parseData = await data.json();
+    console.log(parseData);
+    this.setState({ page: this.state.page + 1, articles: parseData.articles });
+  };
   async componentDidMount() {
     let url =
       "https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=55c7e6a22d60449892bca4b70dca2cb6";
@@ -28,7 +39,6 @@ export default class News extends Component {
     this.setState({ articles: parseData.articles });
   }
   render() {
-   
     return (
       <div>
         <div className="container my-3">
@@ -53,11 +63,20 @@ export default class News extends Component {
               })}
             </div>
             <div className="container d-flex justify-content-between">
-              <button type="button" className="btn btn-dark" onClick={this.handlePrevClick}>
-              &larr; Previous
+              <button
+                type="button"
+                disabled={this.state.page <= 1}
+                className="btn btn-dark"
+                onClick={this.handlePrevClick}
+              >
+                &larr; Previous
               </button>
-              <button type="button" className="btn btn-dark mx-3" onClick={this.handleNextClick}>
-                Next	&rarr;              
+              <button
+                type="button"
+                className="btn btn-dark mx-3"
+                onClick={this.handleNextClick}
+              >
+                Next &rarr;
               </button>
             </div>
           </div>
